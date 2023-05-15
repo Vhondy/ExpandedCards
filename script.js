f828c75a752796d255d03c8f702c5fb41b31dce0
Vhondy <vhondy@gmail.com>
2023-05-15 16:18:59 -0400

    Final updates - querystring, addeventlistener and classlist

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel')      // create a reference to each panel 
 
+panels.forEach(function (i) {                           // loop through each panel
+     
+    i.addEventListener('click',function() {             // add a click event to each panel
+        removeActiveClasses()                           // remove active classes
+        i.classList.add('active')                       // make the clicked on panel active
+    });
+
+});
+
+
+/*
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
+*/
 
+function removeActiveClasses(){
+    panels.forEach(function(panel){
+        panel.classList.remove('active')
+    })
+}
+
+
+/*
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
 }
+*/
